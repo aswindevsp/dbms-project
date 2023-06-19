a9afa20ab82d19daad56338d98cc2f7e050ee043
aswindevsp <aswindevsp@gmail.com>
2023-06-19 20:50:50 +0530

    yolo

--- server/data.sql
+++ server/data.sql
@@ -1,14 +1,38 @@
-psql -U postgres     # login to postgres
+-- Create User table
+CREATE TABLE User (
+    UserID SERIAL PRIMARY KEY,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Email VARCHAR(255),
+    Password VARCHAR(255),
+    PhoneNo VARCHAR(20)
+);
 
-CREATE DATABASE dbms;
+-- Create Game table
+CREATE TABLE Game (
+    GameID SERIAL PRIMARY KEY,
+    Title VARCHAR(255),
+    Description TEXT,
+    Genre VARCHAR(255),
+    Price DECIMAL(10, 2),
+    ReleaseDate DATE,
+    Platform VARCHAR(255)
+);
 
-\c dbms;             # connect to the database
+-- Create Order table
+CREATE TABLE Order (
+    OrderID SERIAL PRIMARY KEY,
+    UserID INTEGER REFERENCES User(UserID),
+    OrderDate DATE,
+    TotalAmount DECIMAL(10, 2),
+    Status VARCHAR(255)
+);
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  phoneNo VARCHAR(255) NOT NULL
-);  
+-- Create OrderItem table
+CREATE TABLE OrderItem (
+    OrderItemID SERIAL PRIMARY KEY,
+    OrderID INTEGER REFERENCES Order(OrderID),
+    GameID INTEGER REFERENCES Game(GameID),
+    Quantity INTEGER,
+    Subtotal DECIMAL(10, 2)
+);
