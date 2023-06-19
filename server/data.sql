26d53960d2baadf2a7bfc0a61fa2e743f5ff82af
aswindevsp <aswindevsp@gmail.com>
2023-06-19 22:52:11 +0530

    yolo

--- server/data.sql
+++ server/data.sql
@@ -1,5 +1,5 @@
 -- Create User table
-CREATE TABLE User (
+CREATE TABLE "User" (
     UserID SERIAL PRIMARY KEY,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
@@ -19,10 +19,10 @@ CREATE TABLE Game (
     Platform VARCHAR(255)
 );
 
--- Create Order table
-CREATE TABLE Order (
+-- Create "Order" table
+CREATE TABLE "Order" (
     OrderID SERIAL PRIMARY KEY,
-    UserID INTEGER REFERENCES User(UserID),
+    UserID INTEGER REFERENCES "User"(UserID),
     OrderDate DATE,
     TotalAmount DECIMAL(10, 2),
     Status VARCHAR(255)
@@ -31,8 +31,34 @@ CREATE TABLE Order (
 -- Create OrderItem table
 CREATE TABLE OrderItem (
     OrderItemID SERIAL PRIMARY KEY,
-    OrderID INTEGER REFERENCES Order(OrderID),
+    OrderID INTEGER REFERENCES "Order"(OrderID),
     GameID INTEGER REFERENCES Game(GameID),
     Quantity INTEGER,
     Subtotal DECIMAL(10, 2)
 );
+
+
+
+-- Insert sample data into User table
+INSERT INTO "User" (FirstName, LastName, Email, Password, PhoneNo)
+VALUES
+    ('John', 'Doe', 'johndoe@example.com', 'password123', '1234567890'),
+    ('Jane', 'Smith', 'janesmith@example.com', 'pass456', '9876543210');
+
+-- Insert sample data into Game table
+INSERT INTO Game (Title, Description, Genre, Price, ReleaseDate, Platform)
+VALUES
+    ('Game 1', 'Description of Game 1', 'Action', 29.99, '2022-01-01', 'Xbox'),
+    ('Game 2', 'Description of Game 2', 'Adventure', 39.99, '2022-02-01', 'PlayStation');
+
+-- Insert sample data into "Order" table
+INSERT INTO "Order" (UserID, OrderDate, TotalAmount, Status)
+VALUES
+    (1, '2022-01-15', 59.98, 'Pending'),
+    (2, '2022-02-20', 79.98, 'Processing');
+
+-- Insert sample data into OrderItem table
+INSERT INTO OrderItem (OrderID, GameID, Quantity, Subtotal)
+VALUES
+    (1, 1, 2, 59.98),
+    (2, 2, 1, 39.99);
