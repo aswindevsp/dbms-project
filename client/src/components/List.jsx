9277a4b95dffb40c341d928bfe0209a9f6b6096f
Adrin7113 <adrinjosect@gmail.com>
2023-06-23 01:08:29 +0530

    Added Close Marker

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -23,15 +23,14 @@ const List = ({ data }) => {
       {showModal && (
         <div className="fixed top-0 min-w-screen min-h-screen bg-white flex justify-center items-center z-50">
           <div className="relative w-5/6 min-h-[600px] bg-black/90 rounded-md p-5 text-white font-semibold">
-            <h1
-              className="text-5xl text-white font-bold absolute top-5 right-5 cursor-pointer"
+            <img
+              className="absolute top-5 right-5 cursor-pointer w-10 h-10"
               onClick={() => {
                 setModalData(null);
                 setShowModal(false);
               }}
-            >
-              *
-            </h1>
+              src="/x.webp"
+            />
             <div className="w-full h-full flex gap-5">
               <div className="min-w-[500px]">
                 <img
